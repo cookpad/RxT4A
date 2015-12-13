a118b499d10e3a173ad7030ec1067f45ca35e88d
FUJI Goro (gfx) <gfuji@cpan.org>
2015-12-13 21:18:48 +0900

    add AndroidSchedulers.serial() and .parallel()
    as interfaces to AsyncTask executors

--- rxt4a/src/main/java/com/cookpad/android/rxt4a/schedulers/AndroidSchedulers.java
+++ rxt4a/src/main/java/com/cookpad/android/rxt4a/schedulers/AndroidSchedulers.java
@@ -1,10 +1,12 @@
 package com.cookpad.android.rxt4a.schedulers;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 
 import rx.Scheduler;
+import rx.schedulers.Schedulers;
 
 /**
  * Set of Android-specific schedulers
@@ -33,5 +35,23 @@ public class AndroidSchedulers {
      */
     public static HandlerScheduler from(@NonNull Handler handler) {
         return new HandlerScheduler(handler);
+    }
+
+    /**
+     * A background scheduler with {@link AsyncTask#SERIAL_EXECUTOR}.
+     *
+     * @return A scheduler which executes tasks in serial
+     */
+    public static Scheduler serial() {
+        return Schedulers.from(AsyncTask.SERIAL_EXECUTOR);
+    }
+
+    /**
+     * A background scheduler with {@link AsyncTask#THREAD_POOL_EXECUTOR}
+     *
+     * @return A scheduler which executes tasks in parallel
+     */
+    public static Scheduler parallel() {
+        return Schedulers.from(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 }
